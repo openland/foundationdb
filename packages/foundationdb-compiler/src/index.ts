07644bb5b0c4accef8f0ababfba918dda145dba3
Steve Korshakov <steve@korshakov.com>
2020-08-05 15:11:41 -0700

    feat: Export programatic API

--- packages/foundationdb-compiler/src/index.ts
+++ packages/foundationdb-compiler/src/index.ts
@@ -1,3 +1,4 @@
 export { SchemaModel, PrimaryKey, PrimaryKeyType, AtomicModel, EventModel, ExtensionModel } from './model';
 export { event, eventStore, declareSchema, atomicBool, atomicInt, primaryKey, entity, allowDelete, field, string, integer, boolean, enumString, array, float, struct, union, optional, uniqueIndex, rangeIndex, json, customDirectory, extension } from './builder';
 export { StringBuilder } from './compiler/StringBuilder';
+export { compile, compileFile } from './compiler';
