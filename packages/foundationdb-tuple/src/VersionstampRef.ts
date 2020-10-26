acdd0bc790539fd9b4a3792ec32e74066109a202
Steve Korshakov <steve@korshakov.com>
2020-10-26 13:31:14 +0300

    feat: Automatic resolving of VersionstampRef

--- packages/foundationdb-tuple/src/VersionstampRef.ts
+++ packages/foundationdb-tuple/src/VersionstampRef.ts
@@ -1,10 +1,27 @@
+import { Versionstamp } from './Versionstamp';
+
 export class VersionstampRef {
     readonly index: Buffer;
+    private _resolved: Versionstamp | null = null;
+
     constructor(index: Buffer) {
         if (index.length !== 2) {
             throw Error('Index must be 2 bytes long');
         }
         this.index = index;
-        Object.freeze(this);
+    }
+
+    get resolved() {
+        if (!this._resolved) {
+            throw Error('Versionstamp is not resolved');
+        }
+        return this._resolved;
+    }
+
+    resolve(vt: Buffer) {
+        if (this._resolved) {
+            throw Error('Versionstamp already resolved');
+        }
+        this._resolved = new Versionstamp(Buffer.concat([vt, this.index]));
     }
 }
--- packages/foundationdb/src/createVersionstampRef.ts
+++ packages/foundationdb/src/createVersionstampRef.ts
@@ -2,5 +2,16 @@ import { Context } from '@openland/context';
 import { getTransaction } from './getTransaction';
 
 export function createVersionstampRef(ctx: Context) {
-    return getTransaction(ctx).allocateVersionstampRef();
+    let tx = getTransaction(ctx);
+    let res = tx.allocateVersionstampRef();
+    // tslint:disable-next-line:no-floating-promises
+    (async () => {
+        try {
+            let vt = await tx.getVersionstamp();
+            res.resolve(vt);
+        } catch (e) {
+            // Ignore
+        }
+    })();
+    return res;
 }
