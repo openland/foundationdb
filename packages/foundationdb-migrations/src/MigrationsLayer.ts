b49e7ec51a09f2edaa160dcd340e5cfe046c57a6
Steve Korshakov <steve@korshakov.com>
2020-12-11 20:33:49 +0300

    feat: Log all errors

--- packages/foundationdb-migrations/src/MigrationsLayer.ts
+++ packages/foundationdb-migrations/src/MigrationsLayer.ts
@@ -17,34 +17,35 @@ export class MigrationsLayer extends BaseLayer {
 
     async didStart() {
         singletonWorker({ name: 'com.openland.layers.migrator', db: this.db }, async (ctx) => {
-            if (this.migrations.length !== 0) {
-                let dir = (await this.db.directories.createOrOpen(ctx, ['com.openland.layers', 'migrator', 'completed']))
-                    .withKeyEncoding(encoders.tuple)
-                    .withValueEncoding(encoders.boolean);
-                let appliedTransactions = await dir.range(ctx, []);
-                let remaining = this.migrations.filter((v) => !appliedTransactions.find((m) => m.key[0] === v.key));
-                if (remaining.length > 0) {
-                    log.log(ctx, 'Remaining migrations: ' + remaining.length);
-                    for (let m of remaining) {
-                        log.log(ctx, 'Starting migration: ' + m.key);
-                        let ctx2 = withLogPath(ctx, m.key);
-                        try {
+            try {
+                if (this.migrations.length !== 0) {
+                    let dir = (await this.db.directories.createOrOpen(ctx, ['com.openland.layers', 'migrator', 'completed']))
+                        .withKeyEncoding(encoders.tuple)
+                        .withValueEncoding(encoders.boolean);
+                    let appliedTransactions = await dir.range(ctx, []);
+                    let remaining = this.migrations.filter((v) => !appliedTransactions.find((m) => m.key[0] === v.key));
+                    if (remaining.length > 0) {
+                        log.log(ctx, 'Remaining migrations: ' + remaining.length);
+                        for (let m of remaining) {
+                            log.log(ctx, 'Starting migration: ' + m.key);
+                            let ctx2 = withLogPath(ctx, m.key);
+
                             await m.migration(ctx2);
-                        } catch (e) {
-                            log.error(ctx, e);
-                            throw e;
+                            await inTx(ctx2, async (ctx3) => {
+                                let ex = await dir.get(ctx3, [m.key]);
+                                if (ex) {
+                                    throw Error('Parallel migrations detected');
+                                }
+                                dir.set(ctx3, [m.key], true);
+                            });
+                            log.log(ctx, 'Completed migration: ' + m.key);
                         }
-                        await inTx(ctx2, async (ctx3) => {
-                            let ex = await dir.get(ctx3, [m.key]);
-                            if (ex) {
-                                throw Error('Parallel migrations detected');
-                            }
-                            dir.set(ctx3, [m.key], true);
-                        });
-                        log.log(ctx, 'Completed migration: ' + m.key);
+                        log.log(ctx, 'All migrations are completed');
                     }
-                    log.log(ctx, 'All migrations are completed');
                 }
+            } catch (e) {
+                log.error(ctx, e);
+                throw e;
             }
             await delay(1500000);
         });
