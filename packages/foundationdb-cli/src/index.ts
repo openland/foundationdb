c7156e7cfb270baec99698dc51d02efea39349db
Steve Kite <steve@korshakov.com>
2021-05-16 13:42:02 +0000

    fix: Make cli ls use multiple transactions

--- packages/foundationdb-cli/src/index.ts
+++ packages/foundationdb-cli/src/index.ts
@@ -18,17 +18,17 @@ program.command('ls')
     .description('Read all directories in database')
     .action(async () => {
         const database = await Database.open();
-        const res = await inTx(rootCtx, async (ctx) => {
-            async function readDirectory(parent: string[]): Promise<any> {
-                let dirs = await database.directories.listAll(ctx, parent.length > 0 ? parent : undefined);
-                let dirres: any = {};
-                for (let d of dirs) {
-                    dirres[d] = await readDirectory([...parent, d]);
-                }
-                return dirres;
+        async function readDirectory(parent: string[]): Promise<any> {
+            let dirs = await inTx(rootCtx, async (ctx) => {
+                return await database.directories.listAll(ctx, parent.length > 0 ? parent : undefined);
+            });
+            let dirres: any = {};
+            for (let d of dirs) {
+                dirres[d] = await readDirectory([...parent, d]);
             }
-            return await readDirectory([]);
-        });
+            return dirres;
+        }
+        const res = await readDirectory([]);
         console.log(treeify.asTree(res, true, true));
     });
 
